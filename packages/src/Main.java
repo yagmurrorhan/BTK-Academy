e777e577026cbb6f833f6ff148891c1e3ccb9752
Yağmur ORHAN <yagmurrorhan@gmail.com>
2022-07-16 11:12:08 +0300

    Add files via upload

---
+++ packages/src/Main.java
@@ -0,0 +1,23 @@
+import matematik.DortIslem;
+import matematik.Logaritma;
+//import matematik.*; -> Bu kullanım, matematik paketi altındaki herşeyi getirir.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adınız:");
+
+        String isim = scanner.nextLine();
+
+        System.out.println("Merhaba " + isim);
+
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(2, 3);
+        Logaritma logaritma = new Logaritma();
+
+    }
+}
